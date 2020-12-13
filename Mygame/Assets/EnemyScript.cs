25d6cb7814b634741c06b373215c9b8ffa24156a
Febrian <59677015+FebrianDev@users.noreply.github.com>
2020-12-13 23:48:51 +0700

    Update player & enemy

---
+++ Mygame/Assets/EnemyScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject bullet, childShoot;
+    float timer;
+    int health = 100;
+
+    public static float scaleX;
+
+    public GameObject player;
+    void Start()
+    {
+        scaleX = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(player != null)
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 0.05f);
+
+        if(transform.position.x < player.transform.position.x)
+        {
+            transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+        }
+        else if (transform.position.x > player.transform.position.x)
+        {
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+
+        if (timer > 0.1f)
+        {
+            Instantiate(bullet, childShoot.transform.position, Quaternion.identity);
+            timer = 0f;
+        }
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "BulletPlayer")
+        {
+            health -= 10;
+        }
+    }
+
+}
---
+++ Mygame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class PlayerMovement : MonoBehaviour
+{
+    //for joystick
+    public Joystick joystick;
+
+    public GameObject bullet,childShoot;
+
+    Animator anim;
+    Rigidbody2D rigid;
+
+    public static float scaleX;
+    public float speed = 10f;
+    public float jump = 400f;
+    float dirX = 0;
+    float dirXJoystick = 0;
+    bool isGround;
+
+    int health = 100;
+    void Start()
+    {
+        scaleX = transform.localScale.x;
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        //for joystick
+        //dirXJoystick = joystick.Horizontal;
+       // transform.Translate(new Vector2(dirXJoystick * speed * Time.deltaTime, 0f));
+
+        //for button
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal");
+        transform.Translate(new Vector2(dirX * speed * Time.deltaTime, 0f));
+
+        if (dirX < 0 || dirXJoystick < -0.2)
+        {
+            anim.SetTrigger("Walk");
+            transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+        }
+        else if (dirX > 0 || dirXJoystick > 0.2)
+        {
+            anim.SetTrigger("Walk");
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            anim.SetTrigger("Stop");
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+
+        if ((CrossPlatformInputManager.GetButtonDown("Jump") || joystick.Vertical > 0.5) && isGround)
+        {
+            rigid.velocity = Vector2.zero;
+            rigid.AddForce(Vector2.up * jump);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+
+        if(collision.gameObject.tag == "BulletEnemy")
+        {
+            health -= 10;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = false;
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bullet, childShoot.transform.position, Quaternion.identity);
+    }
+}
